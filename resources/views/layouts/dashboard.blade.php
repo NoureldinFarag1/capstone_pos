3ffe79f8570b2a65d28dbf99c03fd37b5e8f2fe7
NoureldinFarag1 <115923763+NoureldinFarag1@users.noreply.github.com>
2025-03-10 02:08:03 +0200

    Remove unused script stack from dashboard layout.

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -868,7 +868,6 @@
             }))
         })
     </script>
-    @stack('scripts')
 </body>
 <footer class="bg-gray-100 border-t border-gray-200 py-8">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
