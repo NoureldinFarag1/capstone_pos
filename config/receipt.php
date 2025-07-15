34f3311e7a502f85505c5a32bc0d061be1285cb1
NoureldinFarag1 <115923763+NoureldinFarag1@users.noreply.github.com>
2025-07-15 14:57:12 +0300

    fix: Correct paths for thermal and gift receipt templates in configuration

--- config/receipt.php
+++ config/receipt.php
@@ -1,8 +1,8 @@
 <?php
 
 return array (
-  'thermal_receipt_template' => public_path('/storage/templates/thermal_receipt.txt'),
-  'gift_receipt_template' => public_path('/storage/templates/gift_receipt.txt'),
+  'thermal_receipt_template' => storage_path('/storage/templates/thermal_receipt.txt'),
+  'gift_receipt_template' => storage_path('/storage/templates/gift_receipt.txt'),
   'logo_path' => public_path('/images/logo.png'),
   'store_name' => 'LOCAL HUB',
   'store_slogan' => 'Your favorite local destination',
